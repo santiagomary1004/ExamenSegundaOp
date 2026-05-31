e08652da743de128b2a3ed10efd4f7df11d97e1b
IsraelMerlyn <merlyn142a@gmail.com>
2026-05-31 14:18:23 -0600

    first commit

---
+++ src/main/java/com/israelmerlyn/SistemadeMesadeAyuda/SistemadeMesadeAyudaApplication.java
@@ -0,0 +1,13 @@
+package com.israelmerlyn.SistemadeMesadeAyuda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemadeMesadeAyudaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemadeMesadeAyudaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/israelmerlyn/SistemadeMesadeAyuda/SistemadeMesadeAyudaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.israelmerlyn.SistemadeMesadeAyuda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadeMesadeAyudaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
